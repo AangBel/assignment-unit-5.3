82f45ffee30fed0bda7ea299335ac73c0c46645d
Angelica Beltran <angelica.beltran11@gmail.com>
2023-04-28 20:41:25 -0500

    added 6 different albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,43 @@
 console.log('***** Music Collection *****')
+const collection = [];
+const addToCollection = function(title, artist, yearPublished) {
+
+}
+let firstAlbum = {
+    title: 'Dance Fever',
+    artist: 'Florence and the Machine',
+    yearPublished: 2023, 
+};
+
+collection.push(firstAlbum);
+  console.log(collection);
+
+let secondAlbum = {
+    title: 'Lungs',
+    artist: 'Florence and the Machine',
+    yearPublished: 2009, 
+}; 
+
+let thirdAlbum = {
+    title: 'Un dia normal',
+    artist: 'Juanes',
+    yearPublished: 2002, 
+};
+
+let fourthAlbum = {
+    title: 'Young the Giant',
+    artist: 'Young the Giant',
+    yearPublished: 2011, 
+};
+
+let fifthAlbum = {
+    title: 'This is Why',
+    artist: 'Paramore',
+    yearPublished: 2023, 
+};
+
+let sixthAlbum = {
+    title: 'In our own sweet time',
+    artist: 'Vance Joy',
+    yearPublished: 2022, 
+};
