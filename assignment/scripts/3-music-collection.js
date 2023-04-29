3e4f4b6335358d6472827e13e4f140404fac0624
Angelica Beltran <angelica.beltran11@gmail.com>
2023-04-28 23:25:45 -0500

    i dont know what i am doing, i tried to change the formatting of the albums to the one like in codecademy

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,43 +1,46 @@
 console.log('***** Music Collection *****')
 const collection = [];
 function addToCollection(title, artist, yearPublished) {
-
-}
-let firstAlbum = {
-    title: 'Dance Fever',
-    artist: 'Florence and the Machine',
-    yearPublished: 2023, 
 };
+    let collectionAlbums: {
+    firstAlbum: {
+        title: 'Dance Fever',
+        artist: 'Florence and the Machine',
+        yearPublished: 2023, 
+        },
+
+    secondAlbum: {
+        title: 'Lungs',
+        artist: 'Florence and the Machine',
+        yearPublished: 2009, 
+        },
+
+    thirdAlbum: {
+        title: 'Un dia normal',
+        artist: 'Juanes',
+        yearPublished: 2002, 
+        },
+
+    fourthAlbum: {
+        title: 'Young the Giant',
+        artist: 'Young the Giant',
+        yearPublished: 2011, 
+        },
+
+    fifthAlbum: {
+        title: 'This is Why',
+        artist: 'Paramore',
+        yearPublished: 2023, 
+        },
+
+    sixthAlbum: {
+        title: 'In our own sweet time',
+        artist: 'Vance Joy',
+        yearPublished: 2022, 
+        },
+}
+
+;
 
 collection.push(firstAlbum);
  // console.log(collection);
-
-let secondAlbum = {
-    title: 'Lungs',
-    artist: 'Florence and the Machine',
-    yearPublished: 2009, 
-}; 
-
-let thirdAlbum = {
-    title: 'Un dia normal',
-    artist: 'Juanes',
-    yearPublished: 2002, 
-};
-
-let fourthAlbum = {
-    title: 'Young the Giant',
-    artist: 'Young the Giant',
-    yearPublished: 2011, 
-};
-
-let fifthAlbum = {
-    title: 'This is Why',
-    artist: 'Paramore',
-    yearPublished: 2023, 
-};
-
-let sixthAlbum = {
-    title: 'In our own sweet time',
-    artist: 'Vance Joy',
-    yearPublished: 2022, 
-};
