36dd41905623f197a40097f68744e7ce7968b223
Angelica Beltran <angelica.beltran11@gmail.com>
2023-04-29 19:36:35 -0500

    fk

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -10,21 +10,21 @@ function addToCollection(title, artist, yearPublished) {
   });
 
   addToCollection("Dance Fever", "Florence and the Machine", "2023");
-  console.log(addToCollection);
+  console.log(collection);
 
   addToCollection("Lungs", "Florence and the Machine", "2009");
-  console.log(addToCollection);
+  console.log(collection);
 
   addToCollection("Un Dia Normal", "Juanes", "2002");
-  console.log(addToCollection);
+  console.log(collection);
 
   addToCollection("Mind Over Matter", "Young the Giant", "2014");
-  console.log(addToCollection);
+  console.log(collection);
 
   addToCollection("This is Why", "Paramore", "2023");
-  console.log(addToCollection);
+  console.log(collection);
 
   addToCollection("In our own sweet time", "Vance Joy", "2022");
-  console.log(addToCollection);
+  console.log(collection);
 
 } //end to add to coll.
