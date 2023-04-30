5fd69bb9164c667888679771bcf9ca3559182740
Angelica Beltran <angelica.beltran11@gmail.com>
2023-04-29 19:18:31 -0500

    cleaned up formatting

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,80 +3,28 @@ console.log("***** Music Collection *****");
 const collection = [];
 
 function addToCollection(title, artist, yearPublished) {
+  collection.push({
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  });
 
-    collection.push({
-        title: title,
-        artist: artist,
-        yearPublished: yearPublished,
-    });
+  addToCollection("Dance Fever", "Florence and the Machine", "2023");
+  console.log(addToCollection);
 
+  addToCollection("Lungs", "Florence and the Machine", "2009");
+  console.log(addToCollection);
 
-addToCollection(
-    "Dance Fever",
-    "Florence and the Machine", 
-    "2023",
-)
+  addToCollection("Un Dia Normal", "Juanes", "2002");
+  console.log(addToCollection);
 
-}//end to add to coll. 
+  addToCollection("Mind Over Matter", "Young the Giant", "2014");
+  console.log(addToCollection);
 
-console.log(addToCollection);
+  addToCollection("This is Why", "Paramore", "2023");
+  console.log(addToCollection);
 
-addToCollection(
-    "Lungs",
-    "Florence and the Machine", 
-    "2009",
-)
-console.log(addToCollection);
+  addToCollection("In our own sweet time", "Vance Joy", "2022");
+  console.log(addToCollection);
 
-addToCollection(
-    "Dance Fever",
-    "Florence and the Machine", 
-    "2023",
-)
-console.log(addToCollection);
-
-addToCollection(
-    "Un Dia Normal",
-    "Juanes", 
-    "2002",
-)
-console.log(addToCollection);
-
-addToCollection(
-    "Mind Over Matter",
-    "Young the Giant", 
-    "2014",
-)
-console.log(addToCollection);
-
-addToCollection(
-    "This is Why",
-    "Paramore", 
-    "2023",
-)
-console.log(addToCollection);
-
-addToCollection(
-    "In our own sweet time",
-    "Vance Joy", 
-    "2022",
-)
-console.log(addToCollection);
-
-addToCollection(
-    "Un Dia Normal",
-    "Juanes", 
-    "2002",
-)
-console.log(addToCollection);
-
-}//end to addtocollection
-
-collection.addToCollection("Inour own sweet time","Vance Joy","2022");
-  
-
-  
-
-console.log(collection);
-*/
-//return collection;
+} //end to add to coll.
