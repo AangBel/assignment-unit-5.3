768d23e7b1fbf02b5d00be518270277312896b1d
Angelica Beltran <angelica.beltran11@gmail.com>
2023-04-29 19:58:52 -0500

    steven p is the best, he helped me out for a few minutes on the weekend, i was missing a } at the end and was actually overthinking the way the quesstion was written about returning the console log or whatnot

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -7,24 +7,27 @@ function addToCollection(title, artist, yearPublished) {
     title: title,
     artist: artist,
     yearPublished: yearPublished,
-  });
+  })
+  return collection[collection.length -1];
 
-  addToCollection("Dance Fever", "Florence and the Machine", "2023");
+};
+
+  console.log(addToCollection("Dance Fever", "Florence and the Machine", "2023"));
   console.log(collection);
 
-  addToCollection("Lungs", "Florence and the Machine", "2009");
+  console.log(addToCollection("Lungs", "Florence and the Machine", "2009"));
   console.log(collection);
 
-  addToCollection("Un Dia Normal", "Juanes", "2002");
+  console.log(addToCollection("Un Dia Normal", "Juanes", "2002"));
   console.log(collection);
 
-  addToCollection("Mind Over Matter", "Young the Giant", "2014");
+  console.log(addToCollection("Mind Over Matter", "Young the Giant", "2014"));
   console.log(collection);
 
-  addToCollection("This is Why", "Paramore", "2023");
+  console.log(addToCollection("This is Why", "Paramore", "2023"));
   console.log(collection);
 
-  addToCollection("In our own sweet time", "Vance Joy", "2022");
+  console.log(addToCollection("In our own sweet time", "Vance Joy", "2022"));
   console.log(collection);
 
-} //end to add to coll.
+
